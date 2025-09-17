0e1d773ef10513e4f406708e7adc2505140b84e1
Angelo Garcia <angelo.garcia@stu.ocsb.ca>
2025-09-17 23:21:20 +0000

    added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Angelo Garcia
+# Date: sept. 15, 2025
+# This program displays basic math.
+
+
+def main():
+    print("For a rectangle with a length of 7cm and a width of 5cm:")
+    print()
+    print("The area is: {}cm²".format(7 * 5))
+    print("The perimeter is: {}cm".format(2 * (5 + 3)))
+
+
+if __name__ == "__main__":
+    main()
